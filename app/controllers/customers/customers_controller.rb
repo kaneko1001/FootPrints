6f6648ed46d8d5fbde1d21f668cc953da5977bbb
kaneko1001 <kaneko1001.1995@gmail.com>
2023-06-10 13:11:20 +0000

    [add]customers/edit作成

--- app/controllers/customers/customers_controller.rb
+++ app/controllers/customers/customers_controller.rb
@@ -9,6 +9,19 @@ class Customers::CustomersController < ApplicationController
     @posts = @customer.posts
   end
 
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to customer_path(@customer), notice: "編集に成功しました。"
+    else
+      render :edit
+    end
+  end
+
   private
 
   def customer_params
