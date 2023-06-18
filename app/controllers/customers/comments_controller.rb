6da8e27fd05f9b02df77a8339f5302a337059f49
kaneko1001 <kaneko1001.1995@gmail.com>
2023-06-18 13:13:32 +0000

    コメント非同期化

--- app/controllers/customers/comments_controller.rb
+++ app/controllers/customers/comments_controller.rb
@@ -3,17 +3,21 @@ class Customers::CommentsController < ApplicationController
   before_action :set_comment, only: [:destroy]
 
   def create
-    post = Post.find(params[:post_id])
-    comment = current_customer.comments.new(post_comment_params)
-    comment.post = post
-    comment.save
-    redirect_to post_path(post)
+    @post = Post.find(params[:post_id])
+    @comment = current_customer.comments.new(post_comment_params)
+    @comment.post = @post
+    @comment.save
+    respond_to do |format|
+      format.html { redirect_to post_path(@post) }
+      format.js
+    end
   end
 
   def destroy
-    post = @comment.post
+    @comment = Comment.find(params[:id])
+    @post = @comment.post
     @comment.destroy
-    redirect_to post_path(post)
+    # redirect_to post_path(post)
   end
 
   private
