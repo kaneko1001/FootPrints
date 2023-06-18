c700eaf38e2508d889c6bdbbaca434c0ddd4784b
kaneko1001 <kaneko1001.1995@gmail.com>
2023-06-18 09:50:38 +0000

    [update]いいねの非同期化

--- app/controllers/customers/favorites_controller.rb
+++ app/controllers/customers/favorites_controller.rb
@@ -1,17 +1,23 @@
 class Customers::FavoritesController < ApplicationController
 
+  before_action :set_post, only: [:create, :destroy]
+
   def create
-    post = Post.find(params[:post_id])
-    favorite = Favorite.new(post_id: post.id, customer_id: current_customer.id)
+    @post = Post.find(params[:post_id])
+    favorite = Favorite.new(post_id: @post.id, customer_id: current_customer.id)
     favorite.save
-    redirect_to post_path(post)
   end
 
   def destroy
-    post = Post.find(params[:post_id])
-    favorite = current_customer.favorites.find_by(post_id: post.id)
+    @post = Post.find(params[:post_id])
+    favorite = current_customer.favorites.find_by(post_id: @post.id)
     favorite.destroy
-    redirect_to post_path(post)
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
   end
 
 end
