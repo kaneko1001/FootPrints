bee397a3fb533b6d60a735a6937dc870a1f8c17f
kaneko1001 <kaneko1001.1995@gmail.com>
2023-06-26 04:41:45 +0000

    [add,update]投稿詳細レイアウト,seedの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,15 +13,67 @@ Admin.create!(
   password: 'password',
   password_confirmation: 'password'
   )
+
 puts "------------customer create----------------"
 Customer.destroy_all
-10.times do
-  name = Faker::Name.name
-  truncated_name = name[0, 10]
-  Customer.create!(
-    name: truncated_name,
-    email: Faker::Internet.email,
-    password: 'password',
-    password_confirmation: 'password'
-  )
-end
+customers = Customer.create!(
+  [
+    { email: 'olivia@example.com', name: 'Olivia', password: 'password', profile_image: ActiveStorage::Blob.create_after_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user1.jpg"), filename: "sample-user1.jpg") },
+    { email: 'james@example.com', name: 'James', password: 'password', profile_image: ActiveStorage::Blob.create_after_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user2.jpg"), filename: "sample-user2.jpg") },
+    { email: 'lucas@example.com', name: 'Lucas', password: 'password', profile_image: ActiveStorage::Blob.create_after_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user3.jpg"), filename: "sample-user3.jpg") }
+  ]
+)
+
+puts "------------Post create----------------"
+Post.destroy_all
+Post.create!(
+  [
+    {
+      title: '岩手県に旅行に行きました。',
+      images: [
+        ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post1.jpg"), filename: "sample-post1.jpg"),
+        ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post2.jpg"), filename: "sample-post2.jpg")
+      ],
+      location: [
+        { name: '盛岡八幡宮' },
+        { name: '初駒わんこそば本店' }
+      ],
+      departure_date: Date.new(2023, 6, 25),
+      return_date: Date.new(2023, 6, 30),
+      content: 'わんこそばを食べました',
+      customer_id: customers[0].id
+    },
+
+    {
+      title: '北海道',
+      images: [
+        ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post3.jpg"), filename: "sample-post3.jpg"),
+        ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post4.jpg"), filename: "sample-post4.jpg"),
+        ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post5.jpg"), filename: "sample-post5.jpg")
+      ],
+      location: [
+        { name: '北海道青い池' },
+        { name: 'ケンとメリーの木' },
+        { name: 'セブンスターの木' }
+      ],
+      departure_date: Date.new(2023, 5, 25),
+      return_date: Date.new(2023, 6, 10),
+      content: '北海道に行ってきました！とてもきれいでした。',
+      customer_id: customers[1].id
+    },
+
+    {
+      title: '横浜赤レンガ倉庫に行ってきました。',
+      images: [
+        ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post6.jpg"), filename: "sample-post6.jpg")
+      ],
+      location: [
+        { name: '横浜赤レンガ倉庫' }
+      ],
+      departure_date: Date.new(2023, 6, 10),
+      return_date: Date.new(2023, 6, 10),
+      content: '日帰りで、横浜赤レンガ倉庫に行ってきました。ライトアップされていてとてもきれいでした',
+      customer_id: customers[2].id
+    }
+  ]
+)
