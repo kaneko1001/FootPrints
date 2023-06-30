00a51c6936f7aee84438998b03b8c83886a8b4a7
kaneko1001 <kaneko1001.1995@gmail.com>
2023-06-30 03:34:55 +0000

    [Fix]ページネイション

--- app/controllers/customers/searches_controller.rb
+++ app/controllers/customers/searches_controller.rb
@@ -4,7 +4,7 @@ class Customers::SearchesController < ApplicationController
 
   def search
     @content = params[:content]
-    @customers = Customer.search_active_customers(@content)
-    @posts = Post.joins(:customer).where("customers.is_deleted = ? AND (posts.title LIKE ? OR posts.content LIKE ?)", false, "%#{@content}%", "%#{@content}%")
+    @customers = Customer.search_active_customers(@content).page(params[:page])
+    @posts = Post.joins(:customer).where("customers.is_deleted = ? AND (posts.title LIKE ? OR posts.content LIKE ?)", false, "%#{@content}%", "%#{@content}%").page(params[:page])
   end
 end
