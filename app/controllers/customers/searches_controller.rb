be303634488d91a3d11ca72ab9d1b64bde450023
kaneko1001 <kaneko1001.1995@gmail.com>
2023-07-15 02:18:10 +0000

    [fix]検索機能のバグ修正

--- app/controllers/customers/searches_controller.rb
+++ app/controllers/customers/searches_controller.rb
@@ -4,7 +4,12 @@ class Customers::SearchesController < ApplicationController
 
   def search
     @content = params[:content]
-    @customers = Customer.search_active_customers(@content).page(params[:page])
-    @posts = Post.joins(:customer).where("customers.is_deleted = ? AND (posts.title LIKE ? OR posts.content LIKE ?)", false, "%#{@content}%", "%#{@content}%").page(params[:page])
+    if @content.present?
+      @customers = Customer.search_active_customers(@content).page(params[:page])
+      @posts = Post.joins(:customer).where("customers.is_deleted = ? AND (posts.title LIKE ? OR posts.content LIKE ?)", false, "%#{@content}%", "%#{@content}%").page(params[:page])
+    else
+      @customers = []
+      @posts = []
+    end
   end
 end
